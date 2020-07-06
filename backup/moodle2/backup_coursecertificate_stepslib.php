3831f162cc00c285b20373199b66feb43796f8c6
Mikel Martín <mikel@moodle.com>
2020-07-06 17:20:56 +0200

    Certificate issues backup

--- backup/moodle2/backup_coursecertificate_stepslib.php
+++ backup/moodle2/backup_coursecertificate_stepslib.php
@@ -40,16 +40,35 @@ class backup_coursecertificate_activity_structure_step extends backup_activity_s
      * @return backup_nested_element The structure wrapped by the common 'activity' element.
      */
     protected function define_structure() {
+        global $DB;
+
         // Replace with the attributes and final elements that the element will handle.
         $attributes = ['id'];
         $finalelements = ['name', 'timecreated', 'timemodified', 'intro',
             'introformat', 'template', 'automaticsend', 'expires'];
+
+
         $root = new backup_nested_element('coursecertificate', $attributes, $finalelements);
+
+        // The issues.
+        $issues = new backup_nested_element('issues');
+        $issue = new backup_nested_element('issue', ['id'],
+            ['userid', 'templateid', 'code', 'emailed', 'timecreated', 'expires', 'data', 'component', 'courseid']);
+
+        // Build the tree.
+        $root->add_child($issues);
+        $issues->add_child($issue);
 
         // Define the source tables for the elements.
         $root->set_source_table('coursecertificate', ['id' => backup::VAR_ACTIVITYID]);
 
-        // Define id annotations.
+        // If we are including user info then save the issues.
+        if ($this->get_setting_value('userinfo')) {
+            if (!$DB->get_manager()->table_exists('tool_certificate_issues')) {
+                throw new \dml_exception('tool_certificate_issues table does not exists');
+            }
+            $issue->set_source_table('tool_certificate_issues', ['courseid' => backup::VAR_COURSEID]);
+        }
 
         // Define file annotations.
         $root->annotate_files('mod_coursecertificate', 'intro', null); // This file area hasn't itemid.
--- backup/moodle2/restore_coursecertificate_stepslib.php
+++ backup/moodle2/restore_coursecertificate_stepslib.php
@@ -41,6 +41,12 @@ class restore_coursecertificate_activity_structure_step extends restore_activity
     protected function define_structure(): array {
         $paths = [];
         $paths[] = new restore_path_element('coursecertificate', '/activity/coursecertificate');
+
+        // Check if we want the issues as well.
+        if ($this->get_setting_value('userinfo')) {
+            $paths[] = new restore_path_element('tool_certificate_issue', '/activity/coursecertificate/issues/issue');
+        }
+
         return $this->prepare_activity_structure($paths);
     }
 
@@ -57,6 +63,31 @@ class restore_coursecertificate_activity_structure_step extends restore_activity
         $newitemid = $DB->insert_record('coursecertificate', $data);
         // Immediately after inserting "activity" record, call this.
         $this->apply_activity_instance($newitemid);
+    }
+
+    /**
+     * Handles restoring a tool certificate issue.
+     *
+     * @param stdClass $data the tool certificate data
+     */
+    protected function process_tool_certificate_issue($data) {
+        global $DB;
+
+        $data = (object) $data;
+        $oldid = $data->id;
+
+        $data->courseid = $this->get_courseid();
+        if (!class_exists('\\tool_certificate\\certificate')) {
+            throw new \coding_exception('\\tool_certificate\\certificate class does not exists');
+        }
+        $data->code = \tool_certificate\certificate::generate_code();
+        $data->timecreated = $this->apply_date_offset($data->timecreated);
+
+        if (!$DB->get_manager()->table_exists('tool_certificate_issues')) {
+            throw new \dml_exception('tool_certificate_issues table does not exists');
+        }
+        $newitemid = $DB->insert_record('tool_certificate_issues', $data);
+        $this->set_mapping('tool_certificate_issue', $oldid, $newitemid);
     }
 
     /**
