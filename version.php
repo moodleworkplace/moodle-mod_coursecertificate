5231ec596605810858e654ef9436abe3956c92f1
Marina Glancy <marina@moodle.com>
2021-01-22 17:42:24 +0100

    Add supported moodle version to version.php

--- version.php
+++ version.php
@@ -29,6 +29,7 @@ $plugin->release      = '3.10.1';
 $plugin->version      = 2021011900;
 $plugin->requires     = 2020061502.00;
 $plugin->maturity     = MATURITY_STABLE;
+$plugin->supported    = [39, 310];
 $plugin->dependencies = [
     'tool_certificate' => 2021011900
 ];
