864c69b1696f6282e611827c6a7a0a2ce7705b55
Marina Glancy <marina@moodle.com>
2022-04-20 15:53:10 +0200

    Version bump 4.0dev

--- version.php
+++ version.php
@@ -25,11 +25,11 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component    = 'mod_coursecertificate';
-$plugin->release      = '4.0dev';
-$plugin->version      = 2022031900;
+$plugin->release      = '4.0.0';
+$plugin->version      = 2022042000;
 $plugin->requires     = 2022030800.00;
 $plugin->maturity     = MATURITY_STABLE;
 $plugin->supported    = [400, 400];
 $plugin->dependencies = [
-    'tool_certificate' => 2022031900,
+    'tool_certificate' => 2022042000,
 ];
