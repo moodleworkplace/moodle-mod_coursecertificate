0d5421019e808a2e7f72a2ce47e18d901ca27247
Moodle Workplace <workplace@moodle.com>
2025-06-09 12:41:30 +0000

    Version bump 5.0.1

--- version.php
+++ version.php
@@ -25,11 +25,11 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component    = 'mod_coursecertificate';
-$plugin->release      = '5.0';
-$plugin->version      = 2025041400;
+$plugin->release      = '5.0.1';
+$plugin->version      = 2025061000;
 $plugin->requires     = 2025041400.00;
 $plugin->maturity     = MATURITY_STABLE;
 $plugin->supported    = [500, 500];
 $plugin->dependencies = [
-    'tool_certificate' => 2025041400,
+    'tool_certificate' => 2025061000,
 ];
